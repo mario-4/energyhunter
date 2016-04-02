7ff1839a5f49cc526c14849eb901f5d5824b9e68
Mario Paulo <mariopaulo@appgenesis.com.br>
2016-04-02 19:03:20 -0300

    Comentary

--- EnergyHunter/main.lua
+++ EnergyHunter/main.lua
@@ -7,7 +7,7 @@ display.setStatusBar(display.HiddenStatusBar)
 -- Consider these, global variables that Composer stores for you. 
 -- Later in the project, you can call use/change this variables.
 -- It doesn't matter if it's a string or a number in Lua. I just named them like this for easier understanding.
-
+-- Comment
 composer.setVariable("variableString", "Game Level")
 composer.setVariable("fontSize", 64)
 composer.gotoScene("screens.logo", "crossFade", 500)
